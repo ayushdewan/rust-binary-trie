a1edddbfeeb0c3696d616ce45e2b215658aaa4dc
Ayush Dewan <ayushdewan@ayushs-mbp.attlocal.net>
2022-05-20 20:49:55 -0700

    delete function implemented w/o path cleaning

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ impl Tree {
     fn print(&self, prefix: &str) {
         if let &Tree::Empty(true) = self {
             println!("{}", prefix);
-            return;
         }
         if let &Tree::Node(ref l, ref r) = self {
             let l_str = &(String::from(prefix) + "0");
@@ -37,16 +36,19 @@ impl Set {
     }
 
     fn add(&mut self, item: u8) {
+        // keep track of current pointer
         let mut curr = &mut self.rep;
-
         for i in (0..8).rev() {
+            // extract i'th bit
             let indexer = 1 << i;
             let dir = if (item & indexer) == 0 { 0 } else { 1 };
 
+            // create new path if we hit a dead-end
             if let &mut Tree::Empty(_) = curr {
                 *curr = Tree::Node(Box::new(Tree::Empty(false)), Box::new(Tree::Empty(false)));
             }
 
+            // have pointer follow path
             if let &mut Tree::Node(ref mut l, ref mut r) = curr {
                 curr = if dir == 0 { l } else { r };
             }
@@ -56,6 +58,32 @@ impl Set {
         if let &mut Tree::Empty(false) = curr {
             self.len += 1;
             *curr = Tree::Empty(true);
+        }
+    }
+
+    fn delete(&mut self, item: u8) {
+        // keep track of current pointer
+        let mut curr = &mut self.rep;
+        for i in (0..8).rev() {
+            // extract i'th bit
+            let indexer = 1 << i;
+            let dir = if (item & indexer) == 0 { 0 } else { 1 };
+
+            // do nothing if we hit a dead-end
+            if let &mut Tree::Empty(_) = curr {
+                return;
+            }
+
+            // have pointer follow path
+            if let &mut Tree::Node(ref mut l, ref mut r) = curr {
+                curr = if dir == 0 { l } else { r };
+            }
+        }
+
+        // check if current element is in tree
+        if let &mut Tree::Empty(true) = curr {
+            *curr = Tree::Empty(false);
+            self.len -= 1;
         }
     }
 }
@@ -75,5 +103,14 @@ fn main() {
     c.add(19);
     c.add(32);
     c.add(0);
+    c.print();
+
+    println!("--------");
+    c.delete(0);
+    c.print();
+
+    println!("--------");
+    c.add(8);
+    c.delete(32);
     c.print();
 }
